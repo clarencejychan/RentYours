812a2362883dd2c869c8ac42a84e29fc8527b745
jerushachua <jerushachua@gmail.com>
2017-07-22 01:53:14 -0600

    made the navbar sticky

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -4,41 +4,33 @@ import $ from 'jquery';
 import './style.css';
 
 class Home extends Component {
-  componentDidMount(){
-    $('#nav-wrapper').height($("#nav").height());
-    $('#nav').affix({
-      offset: { top: $('#nav').offset().top}
-    });
-  }
   render() {
     return (
-      <div id="nav-wrapper">
-        <Navbar inverse staticTop collapseOnSelect id="nav" clas="navbar">
-          <Navbar.Header class="navbar-header">
-             <Navbar.Brand>
-               <a href="#">React-Bootstrap</a>
-             </Navbar.Brand>
-             <Navbar.Toggle />
-           </Navbar.Header>
-           <Navbar.Collapse>
-             <Nav>
-               <NavItem eventKey={1} href="#">Link</NavItem>
-               <NavItem eventKey={2} href="#">Link</NavItem>
-               <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                 <MenuItem eventKey={3.1}>Action</MenuItem>
-                 <MenuItem eventKey={3.2}>Another action</MenuItem>
-                 <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                 <MenuItem divider />
-                 <MenuItem eventKey={3.3}>Separated link</MenuItem>
-               </NavDropdown>
-             </Nav>
-             <Nav pullRight>
-               <NavItem eventKey={1} href="#">Link Right</NavItem>
-               <NavItem eventKey={2} href="#">Link Right</NavItem>
-             </Nav>
-           </Navbar.Collapse>
-        </Navbar>
-      </div>
+      <Navbar inverse staticTop collapseOnSelect fixedTop id="nav" className="navbar">
+        <Navbar.Header className="navbar-header">
+          <Navbar.Brand>
+            <a href="#">React-Bootstrap</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem eventKey={1} href="#">Link</NavItem>
+            <NavItem eventKey={2} href="#">Link</NavItem>
+          <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+            <MenuItem eventKey={3.1}>Action</MenuItem>
+            <MenuItem eventKey={3.2}>Another action</MenuItem>
+            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey={3.3}>Separated link</MenuItem>
+          </NavDropdown>
+          </Nav>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="#">Link Right</NavItem>
+            <NavItem eventKey={2} href="#">Link Right</NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
   }
 };
