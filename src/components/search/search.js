54d2ace0f28dd9aa2d95ea9bad439f3d256cc412
Clarence Chan <clarencejy1234@gmail.com>
2017-07-25 22:54:47 -0600

    search styling

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import { FormGroup, InputGroup, Button, FormControl } from 'react-bootstrap';
+import { Row, Col, Button, InputGroup, FormControl } from 'react-bootstrap';
 import SearchButton from 'react-icons/lib/go/search';
 import './style.scss';
 
 class Search extends Component {
   render() {
     return (
-      <FormGroup bsSize="large">
-        <InputGroup>
-          <InputGroup.Button>
-            <Button className="search-icon">
-            <SearchButton />
-            </Button>
-          </InputGroup.Button>
-          <FormControl type="text"/>
-        </InputGroup>
-      </FormGroup>
+      <Row>
+        <Col xs={20} md={12}>
+          <InputGroup bsSize="large">
+            <FormControl className="search-form" placeholder="Enter text"/>
+              <InputGroup.Button bsSize="large">
+                <Button bsSize="large" className="search-button">
+                  <SearchButton size={'18px'}/>
+                </Button>
+              </InputGroup.Button>
+          </InputGroup>
+        </Col>
+	   </Row>
     );
   }
 };
