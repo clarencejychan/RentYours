634f5aadf113ca4cd45ffc7bfc9ca42bde351f14
Clarence Chan <clarencejy1234@gmail.com>
2017-08-12 13:15:11 -0600

    more styling changes

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
 import $ from 'jquery';
 
 import Search from '../search/search';
@@ -37,6 +39,8 @@ class Home extends Component {
       <div className="homepage">
         <div className="homepage-container">
           <div className="homepage-container-main">
+
+            {/* Navbar */}
             <Navbar collapseOnSelect className={this.state.scrollPast} id="navbar" role="navigation">
               <Navbar.Header>
                 <Navbar.Brand>
@@ -45,46 +49,51 @@ class Home extends Component {
                 <Navbar.Toggle />
               </Navbar.Header>
               <Navbar.Collapse>
-                <Nav>
-                  <NavItem eventKey={1} href="#">Link</NavItem>
-                  <NavItem eventKey={2} href="#">Link</NavItem>
-                <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                  <MenuItem eventKey={3.1}>Action</MenuItem>
-                  <MenuItem eventKey={3.2}>Another action</MenuItem>
-                  <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                  <MenuItem divider />
-                  <MenuItem eventKey={3.3}>Separated link</MenuItem>
-                </NavDropdown>
-                </Nav>
                 <Nav pullRight>
-                  <NavItem id="link-top-left" eventKey={1} href="#">Log In</NavItem>
-                  <NavItem id="link-top-right" eventKey={2} href="#">Sign Up</NavItem>
+                  <LinkContainer to="/test" className="nav-item">
+                    <NavItem id="list-items">List Items</NavItem>
+                  </LinkContainer>
+                  <LinkContainer to="/listings" className="nav-item">
+                    <NavItem id="browse-items" >Browse Items</NavItem>
+                  </LinkContainer>
+                  <LinkContainer to="/login" className="nav-item">
+                    <NavItem id="login">Log In</NavItem>
+                  </LinkContainer>
+                  <LinkContainer to="/signup" className="nav-item">
+                    <NavItem id="sign-up">Sign Up</NavItem>
+                  </LinkContainer>
                 </Nav>
               </Navbar.Collapse>
             </Navbar>
+
+            {/* Home Page Hero*/}
             <div className="homepage-inner-container">
-              <div className="logo-container">
-                <h1 className="logo-text">RentYours</h1>
-              </div>
-              <div className="footer-text">
-                blurb hurr durr
-                <div className="example-icon" />
-              </div>
-              <div className="search-bar">
-                <Search />
+              <div className="homepage-inner-container-contents">
+                <div className="logo-container">
+                  <h1 className="logo-text">RentYours</h1>
+                </div>
+                <div className="footer-text">
+                  blurb hurr durr
+                  <div className="example-icon" />
+                </div>
+                <div className="search-bar">
+                  <Search />
+                </div>
               </div>
             </div>
-          </div>
-        </div>
 
-        <div className="image-container">
-          <img id="img-1" src="http://cdn.wonderfulengineering.com/wp-content/uploads/2014/10/simple-wallpaper-13.jpg"></img>
-        </div>
-        <div className="image-container">
-          <img id="img-2" src="http://wallpapercave.com/wp/4SSFz3R.png"></img>
-        </div>
-        <div className="image-container">
-          <img id="img-3" src="https://hdwallsource.com/img/2014/9/simple-backgrounds-17277-17833-hd-wallpapers.jpg"></img>
+
+            <div className="image-container">
+              <img id="img-1" src="http://cdn.wonderfulengineering.com/wp-content/uploads/2014/10/simple-wallpaper-13.jpg"></img>
+            </div>
+            <div className="image-container">
+              <img id="img-2" src="http://wallpapercave.com/wp/4SSFz3R.png"></img>
+            </div>
+            <div className="image-container">
+              <img id="img-3" src="https://hdwallsource.com/img/2014/9/simple-backgrounds-17277-17833-hd-wallpapers.jpg"></img>
+            </div>
+
+          </div>
         </div>
       </div>
     );
--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
 
+
+//let store = createStore();
+
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  //<Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  //</Provider>,
   document.getElementById('app')
 );
