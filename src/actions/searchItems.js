d6dc851ce96eb95b2193c9ea8dbbb89c1dfc9c4e
Clarence Chan <clarencejy1234@gmail.com>
2017-09-01 12:08:09 -0400

    action creators

--- src/actions/searchItems.js
+++ src/actions/searchItems.js
@@ -2,7 +2,42 @@ import {REQUESTING_ITEMS_INFO, REQUEST_ITEMS_INFO_SUCCESS, REQUEST_ITEMS_INFO_FA
 import fetch from 'isomorphic-fetch';
 
 // REQUEST ITEMS ACTION
+function requestItems(itemName) {
+  return {
+    type: REQUESTING_ITEMS_INFO,
+    searchQuery: itemName
+  }
+}
 
 // REQUEST ITEMS SUCCESS ACTION
 
+function requestItemsSuccess(itemName, itemInfo) {
+  return {
+    type: REQUEST_ITEMS_INFO_SUCCESS,
+
+  }
+}
+
 // REQUEST ITEMS FAILURE ACTION
+
+function requestItemsFailure(itemName) {
+  return {
+    type: REQUEST_ITEMS_INFO_FAILURE,
+    searchQuery: itemName
+  }
+}
+
+
+export default function getSearchItems(itemName)) {
+  return dispatch => {
+    // Show that it is requesting, will be updated after.
+    dispatch(requestItems(itemName));
+    fetch('')
+    .then(
+      reponse => response.json(),
+      error => dispatch(reqestItemsFailure(itemName))
+    ).then(json =>
+      dispatch(requestItemsSuccess(itemName, json));
+    )
+  }
+}
--- src/reducers/searchReducer.js
+++ src/reducers/searchReducer.js
@@ -1,22 +1,23 @@
 import {REQUESTING_ITEMS_INFO, REQUEST_ITEMS_INFO_SUCCESS, REQUEST_ITEMS_INFO_FAILURE} from '../actions/types';
 
-export default function(state = { isFetching: false, error: false, items:[] }, action) {
+export default function(state = { isFetching: false, error: false, searchQuery:"", items:[] }, action) {
   switch (action.type) {
     case REQUESTING_ITEMS_INFO:
       return Object.assign({}, state, {
-        isFetching: true
+        isFetching: true,
+        searchQuery: action.itemName
       })
     case REQUEST_ITEMS_INFO_SUCCESS:
       return Object.assign({}, state, {
         isFetching: false,
         error: false,
-        searchQuery: action.itemName,
         items: action.posts,
         lastUpdated: action.receivedAt
       })
     case REQUEST_ITEMS_INFO_FAILURE:
       return Object.assign({}, state, {
         isFetching: false,
+        searchQuery: action.itemName
         error: true
       })
     default:
