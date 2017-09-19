1b08e6bd9136b9cdc90abad577fef31ad59eba72
Clarence Chan <clarencejy1234@gmail.com>
2017-09-18 23:29:43 -0400

    Able to send links to people

--- src/components/searchResults/searchResults.js
+++ src/components/searchResults/searchResults.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import queryString from 'query-string'
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import Item from './item/item';
+import getSearchItems from '../../actions/searchItemsAction';
 
 // Container Component
 class SearchResults extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    // Here I know that someone may be trying to send link to somebody else.
+    if (this.props.items && !this.props.items.length && this.props.location.search) {
+      let searchText = queryString.parse(this.props.location.search);
+      if ('item-name' in searchText) {
+        // Have to use key notation cause of hyphen
+        this.props.getSearchItems(searchText['item-name']);
+      }
+      // Need to dispatch an action
+    }
+  }
+
   render() {
     console.log(this.props.items);
     return (
@@ -14,6 +33,12 @@ class SearchResults extends Component {
   }
 }
 
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({
+    getSearchItems: getSearchItems
+  }, dispatch);
+}
+
 function mapStateToProps(state) {
   return {
     // Gets the list of fetched items
@@ -21,4 +46,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps, null)(SearchResults);
+export default connect(mapStateToProps, mapDispatchToProps)(SearchResults);
